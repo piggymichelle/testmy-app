fa337b394c54af42e35b2ba2ca236fd99976feb4
piggymichelle <spotofsky@outlook.com>
2020-03-19 22:01:44 +0800

    delete change4

--- src/Myarea.js
+++ src/Myarea.js
@@ -10,7 +10,6 @@ const Myarea = props => {
             <p>change3</p>
             <p>disco</p>
             <p>echo</p>
-            <p>change4</p>
             <h1>{props.nameList}</h1>
             <h1>{props.ageList}</h1>
         </div>);
