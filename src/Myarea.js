1df3c7651f89b2ac3ab61df07e7fad1263d791c7
piggymichelle <spotofsky@outlook.com>
2020-03-29 23:46:36 +0800

    add a banana

--- src/Myarea.js
+++ src/Myarea.js
@@ -7,6 +7,7 @@ const Myarea = props => {
             <p>change3</p>
             <p>change4</p>
             <p>apple</p>
+            <p>banana</p>
             <h1>{props.nameList}</h1>
             <h1>{props.ageList}</h1>
         </div>);
