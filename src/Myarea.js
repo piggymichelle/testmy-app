74114b96cc4baf447d4307887a4d62f05a3d4e26
piggymichelle <spotofsky@outlook.com>
2021-07-17 13:00:26 +0800

    cherry pick

--- src/Myarea.js
+++ src/Myarea.js
@@ -6,6 +6,7 @@ const Myarea = props => {
             <p>change2</p>
             <p>change3</p>
             <p>change4</p>
+            <p>apple now cherry pick</p>
             <h1>{props.nameList}</h1>
             <h1>{props.ageList}</h1>
         </div>);
